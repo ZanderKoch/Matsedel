6851414fccc6c5b337c0b529845e3fcfcdd430bd
SakuyaYae <darkreimu@outlook.com>
2021-10-18 14:42:11 +0200

    added mor inserts to test whit
    bug solved?

--- menu_dml.sql
+++ menu_dml.sql
@@ -6,4 +6,12 @@ INSERT INTO menu(serving_date, description) VALUES("2021-12-24", "Lutfisk o pota
 
 INSERT INTO menu(serving_date, description) VALUES("", "Grillad ryggbiff serveras med hasselbacks potatis och rödvinsås" );
 
-SELECT * FROM menu;
+INSERT INTO menu(serving_date, description) VALUES("2021-01-02", "" );
+
+INSERT INTO menu(serving_date, description) VALUES("", "" );
+
+INSERT INTO menu(serving_date, description) VALUES("", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec ligula laoreet, placerat enim suscipit, scelerisque augue. Nunc vitae ligula sollicitudin tellus lobortis consectetur vitae eu mauris. Duis maximus sollicitudin ante. Pellentesque pharetra ornare lorem quis euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec ullamcorper libero euismod lobortis placerat. Pellentesque ante arcu, semper id orci eu, congue aliquet dolor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin odio quam, imperdiet vitae purus et, maximus varius mi. Nullam a felis egestas, egestas nisl sit amet, auctor ante. Nunc vel egestas leo. Nulla et elit in urna pharetra ultrices." );
+
+INSERT INTO menu(serving_date, description) VALUES("2021-11-32", "te" );
+
+SELECT * FROM menu ORDER BY serving_date ASC;
