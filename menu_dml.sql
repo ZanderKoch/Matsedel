75a1c3c081a52c2464da6122da34100529e37890
SakuyaYae <darkreimu@outlook.com>
2021-10-18 14:05:24 +0200

    Added database files
    Database has a bug

---
+++ menu_dml.sql
@@ -0,0 +1,9 @@
+INSERT INTO menu(serving_date, description) VALUES("2021-12-04", "Miso soup whit white bread" );
+
+INSERT INTO menu(serving_date, description) VALUES("2021-05-08", "Pinapple cake whit ice cream and numerus berrys" );
+
+INSERT INTO menu(serving_date, description) VALUES("2021-12-24", "Lutfisk o potatis serveras med vitsås och fleskaflåt" );
+
+INSERT INTO menu(serving_date, description) VALUES("", "Grillad ryggbiff serveras med hasselbacks potatis och rödvinsås" );
+
+SELECT * FROM menu;
