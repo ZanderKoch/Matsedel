c8aa65fc1085aeac7dc3b163c0da9e5c06b37de6
02Patrick02 <paku02001@utb.vaxjo.se>
2021-10-18 16:00:27 +0200

    jsoup

--- menu/src/main/java/nu/te4/menu/webb_scraping/Webb_scraping.java
+++ menu/src/main/java/nu/te4/menu/webb_scraping/Webb_scraping.java
@@ -5,15 +5,30 @@
  */
 package nu.te4.menu.webb_scraping;
 
+import java.io.IOException;
+/*import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+*/
 /**
  *
  * @author Elev
  */
 public class Webb_scraping {
     
-     public static void main(String[] args) {
-         try{
+    public void scraping(){
+        /*   try{
               Document doc = Jsoup.connect("https://mpi.mashie.com/public/app/V%C3%A4xj%C3%B6%20kommun%20ny/6f5fa240").get();
+              Elements body = doc.select("h3");
+
+              System.out.println("Title");
+               System.out.println(body.get(0).html());
+               System.out.println("");
+               
          }
-     }
+          catch(IOException ex){
+            System.out.println("Error fetching focument!");
+        } 
+*/
+    }
 }
