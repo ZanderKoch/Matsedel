0c3d04be7938b9ffdcf91baa163d69a03baf77dc
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2021-10-19 12:13:55 +0200

    implemented getWeek()

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
+/**
+ * @file script file for a html page that shows menus
+ * @author Zander Koch
+ * @version 1
+ */
+
+
+//////////////////////////////
+//Externally sourced scripts//
+//////////////////////////////
+//i can't figure out how to do this with imports and exports
+
+// This script is released to the public domain and may be used, modified and
+// distributed without restrictions. Attribution not necessary but appreciated.
+// Source: https://weeknumber.com/how-to/javascript
+
+// Returns the ISO week of the date.
+function getWeek(){
+    let currentDate = Date.now()
+    let date = new Date(currentDate);
+    date.setHours(0, 0, 0, 0);
+    // Thursday in current week decides the year.
+    date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+    // January 4 is always in week 1.
+    let week1 = new Date(date.getFullYear(), 0, 4);
+    // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+    return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+                          - 3 + (week1.getDay() + 6) % 7) / 7);
+  }
 ////////////////
 //Declarations//
 ////////////////
+
+
 
 /**
  * @type {String}
@@ -19,7 +50,11 @@ let currentWeekNumber;
 ///////////////////
 
 todayDate = getCurrentDateString();
-console.log(todayDate);
+console.log(todayDate); //debug
+
+currentWeekNumber = getWeek();
+console.log(currentWeekNumber)
+
 
 ///////////
 //Methods//
@@ -33,7 +68,6 @@ console.log(todayDate);
 function getCurrentDateString(){
     //saving the current time as date
     let date = new Date(Date.now());
-    date.setDate(1);
 
     //getting the year, month, and day of date and saving them 
     let year = date.getFullYear();
