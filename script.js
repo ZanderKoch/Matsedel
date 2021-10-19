4d11202f0accb19768a05a3483305ed751947cbc
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2021-10-19 09:47:07 +0200

    added declarations

---
+++ script.js
@@ -0,0 +1,17 @@
+////////////////
+//Declarations//
+////////////////
+
+/**
+ * @type {Date}
+ */
+let todayDate;
+
+/**
+ * the current week of the year as an int
+ * @type {Number} must be an integer
+ */
+let currentWeekNumber;
+
+
+
