c9fed08898e40ef8b59f6bb94497a2239f53aad4
02Patrick02 <paku02001@utb.vaxjo.se>
2021-10-20 13:40:04 +0200

    ändra path till api/test

--- src/main/java/nu/te4/matsedel/resources/JavaEE8Resource.java
+++ src/main/java/nu/te4/matsedel/resources/JavaEE8Resource.java
@@ -3,18 +3,21 @@ package nu.te4.matsedel.resources;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+import Web_Scraping.Web_Scraping;
+import java.text.ParseException;
 
 /**
  *
  * @author 
  */
-@Path("javaee8")
+@Path("test")
 public class JavaEE8Resource {
     
     @GET
-    public Response ping(){
+    public Response ping() throws ParseException{
+        
         return Response
-                .ok("ping")
+                .ok(new Web_Scraping().scraping())
                 .build();
     }
 }
