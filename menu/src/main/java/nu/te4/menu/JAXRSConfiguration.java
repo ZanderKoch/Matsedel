d10ba0e36deb124aab972e478eddea7485c8abcd
SakuyaYae <89073955+SakuyaYae@users.noreply.github.com>
2021-10-25 15:21:31 +0200

    Delete menu directory

--- menu/src/main/java/nu/te4/menu/JAXRSConfiguration.java
+++
@@ -1,9 +0,0 @@
-package nu.te4.menu;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class JAXRSConfiguration extends Application {
-
-}
